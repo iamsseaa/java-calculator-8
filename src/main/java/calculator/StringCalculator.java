0db6c7f6b765d04e2ed5786729122470a6a8ce12
iamsseaa <homingsea@gmail.com>
2025-10-17 21:01:44 +0900

    feat: 빈 문자열 입력 처리 로직 추가

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class StringCalculator {
+
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
