75483fe2bfd4b22b5c5d75a14e7f0f4a7ecfddae
iamsseaa <homingsea@gmail.com>
2025-10-17 21:04:18 +0900

    feat: 결과 출력 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,8 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = camp.nextstep.edu.missionutils.Console.readLine();
+        int result = StringCalculator.add(input);
+        System.out.println("결과 : " + result);
     }
 }
